7ad68ea0a6a752e69d925d134ea556a61a9ad3a9
dacup72 <dacup72@gmail.com>
2018-10-05 16:49:39 -0600

    Added nav bar and materialize

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from "./pages/Main";
 
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <Main />
       </div>
     );
---
+++ src/components/FriendCard/FriendCard.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "./FriendCard.css";
+
+const FriendCard = props => {
+  return (
+    <Fragment>
+      
+
+    </Fragment>
+  )
+}
+
+export default FriendCard;
