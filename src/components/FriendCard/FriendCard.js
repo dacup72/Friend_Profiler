1320fbd52f9e73aca2f9ab53bf46b2d377fe041f
dacup72 <dacup72@gmail.com>
2018-10-09 10:38:21 -0600

    small css fixes

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
-const FriendCard = ({ data, data: { picture, name } }) => (
+const FriendCard = ({ data, data: { picture, name } }) => {
+  const firstName = name.split(" ")[0];
+  const lastName = name.split(" ")[1];
+  return (
   <Fragment>
-    <div className="col s6 m3">
+    <div className="col s12 m6 l4 xl3">
       <div className="card">
         <div className="card-image">
           <img src={picture} alt="Friend" />
-          <span className="card-title">{name}</span>
         </div>
         <div className="card-content">
-          <h3>{name}</h3>
+          <h3>{firstName}</h3>
+          <h3>{lastName}</h3>
           <Link to={{ pathname: "/map", state: { data } }}>
             <button>View Map</button>
           </Link>
@@ -21,6 +24,7 @@ const FriendCard = ({ data, data: { picture, name } }) => (
       </div>
     </div>
   </Fragment>
-);
+  );
+};
 
 export default FriendCard;
